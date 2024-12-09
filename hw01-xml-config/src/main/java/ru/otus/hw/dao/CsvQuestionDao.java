2b540f1d1534393eb218b5bbbbb1167b95df3f95
ilnur.nasibullin <ilnur.nasibullin@x5.ru>
2024-12-09 11:51:02 +0300

    HW01 Доработал классы и контекст

--- hw01-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw01-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -1,9 +1,19 @@
 package ru.otus.hw.dao;
 
+import com.opencsv.CSVReader;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
 import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,7 +27,22 @@ public class CsvQuestionDao implements QuestionDao {
         // https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
         // Использовать QuestionReadException
         // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
+        String testFileName = fileNameProvider.getTestFileName();
 
-        return new ArrayList<>();
+        URL resource = getClass().getClassLoader().getResource(testFileName);
+        List<QuestionDto> questionDtos;
+        try {
+            FileReader reader = new FileReader(resource.getFile());
+            questionDtos = new CsvToBeanBuilder(reader)
+                    .withType(QuestionDto.class)
+                    .withSkipLines(1)
+                    .withSeparator(';')
+                    .build()
+                    .parse();
+        } catch (FileNotFoundException e) {
+            throw new QuestionReadException(testFileName, e);
+        }
+
+        return questionDtos.stream().map(QuestionDto::toDomainObject).toList();
     }
 }
--- hw01-xml-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw01-xml-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -1,16 +1,33 @@
 package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Question;
 
+import java.util.List;
+
+@Setter
 @RequiredArgsConstructor
 public class TestServiceImpl implements TestService {
 
     private final IOService ioService;
+    private QuestionDao questionDao;
 
     @Override
     public void executeTest() {
         ioService.printLine("");
         ioService.printFormattedLine("Please answer the questions below%n");
         // Получить вопросы из дао и вывести их с вариантами ответов
+        List<Question> questions = questionDao.findAll();
+        for (Question question : questions) {
+            ioService.printLine(question.text());
+            for (Answer answer : question.answers()) {
+                ioService.printLine(answer.text());
+            }
+            ioService.printLine("");
+        }
     }
+
 }
