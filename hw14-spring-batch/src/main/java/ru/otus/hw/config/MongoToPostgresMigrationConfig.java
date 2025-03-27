a65fcce6cae8434a8f292602b92c62b36b9ed09a
ilnur.nasibullin <ilnur.nasibullin@x5.ru>
2025-03-27 17:12:23 +0300

    HW14 Spring Batch

---
+++ hw14-spring-batch/src/main/java/ru/otus/hw/config/MongoToPostgresMigrationConfig.java
@@ -0,0 +1,41 @@
+package ru.otus.hw.config;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MongoToPostgresMigrationConfig {
+
+    private static final String MIGRATE_MONGO_TO_POSTGRES_JOB_NAME = "migrateMongoToPostgresJob";
+
+    @Autowired
+    private JobRepository jobRepository;
+
+    @Autowired
+    private AuthorMigrationConfig authorMigrationConfig;
+
+    @Autowired
+    private GenreMigrationConfig genreMigrationConfig;
+
+    @Autowired
+    private BookMigrationConfig bookMigrationConfig;
+
+    @Autowired
+    private CommentMigrationConfig commentMigrationConfig;
+
+    @Bean
+    public Job migrateMongoToPostgresJob() {
+        JobBuilder jobBuilder = new JobBuilder(MIGRATE_MONGO_TO_POSTGRES_JOB_NAME, jobRepository);
+
+        return jobBuilder.start(authorMigrationConfig.migrateAuthorsStep())
+                .next(genreMigrationConfig.migrateGenresStep())
+                .next(bookMigrationConfig.migrateBooksStep())
+                .next(commentMigrationConfig.migrateCommentsStep())
+                .build();
+    }
+
+}
---
+++ hw14-spring-batch/src/main/resources/drop-batch-tables.sql
@@ -0,0 +1,6 @@
+TRUNCATE TABLE batch_job_execution CASCADE;
+TRUNCATE TABLE batch_job_execution_context CASCADE;
+TRUNCATE TABLE batch_job_execution_params CASCADE;
+TRUNCATE TABLE batch_job_instance CASCADE;
+TRUNCATE TABLE batch_step_execution CASCADE;
+TRUNCATE TABLE batch_step_execution_context CASCADE;
